d9154336c8d4f9a009ec38994c5ead313db89e0e
iremkaraoglu <iremk97@gmail.com>
2026-05-15 08:13:42 +0300

    Improve accessibility labels in Speakers

--- MythConf/MythConf26/Speakers/SocialLinksView.swift
+++ MythConf/MythConf26/Speakers/SocialLinksView.swift
@@ -20,6 +20,7 @@ struct SocialLinksView: View {
                             .padding(.horizontal, 4)
                     }
                     .contentShape(.rect)
+                    .accessibilityLabel("\(item.socialType.capitalized), opens in browser")
                 }
             }
         }
--- MythConf/MythConf26/Speakers/TalkSummaryView.swift
+++ MythConf/MythConf26/Speakers/TalkSummaryView.swift
@@ -23,5 +23,7 @@ struct TalkSummaryView: View {
             .foregroundStyle(.secondary)
         }
         .padding(.vertical, 4)
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel("\(viewModel.talkTitleFrom(talkID: talkID)), \(session.timeRange), in \(viewModel.locationNameFrom(talkID: talkID))")
     }
 }
